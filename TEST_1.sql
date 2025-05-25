44abd68019c6ddc6310b0f584eb9c89539e68554
Sabari Raj P <143380285+sabarirajpazhani@users.noreply.github.com>
2025-05-25 18:55:39 +0530

    Update TEST_1.sql

--- TEST_1.sql
+++ TEST_1.sql
@@ -29,11 +29,50 @@ insert into Employee (empid, empName, empSalary,deptID) values
 select * from employee;
 select * from Department;
 
+--1. Write a SQL query to get the names and salaries of the top 3 highest-paid employees from the Employee table.
+select top 3 empName, empSalary from Employee
+order by empSalary DESC;
 
--- Write a SQL query to display all employees who belong to the 'HR' department using JOINs (Employee and Department tables).
+--2. Write a SQL query to display all employees who belong to the 'HR' department using JOINs (Employee and Department tables).
 select emp.empName, dept.deptName
 from Employee emp
 inner join Department dept
 on emp.deptID = dept.deptID
 where dept.deptName = 'HR'
+
+--3. Display department-wise total salary using aggregation.
+select dept.deptName , SUM(emp.empSalary) as TotalSalary 
+FROM Employee emp
+right join Department dept
+on emp.deptID = dept.deptID
+group by deptName;
+
+--4. Fetch employee names whose name starts with ‘S’ and ends with ‘a’.
+--for this question i will update my table employee name
+update Employee
+set 
+empName = case
+	when empid = 1 then 'Abilash'
+	when empid = 2 then 'Subha'
+	when empid = 3 then 'Sharmila'
+	when empid = 4 then 'Thamizh'
+	when empid = 5 then 'Akash'
+	else
+	empName
+end;
+
+select empName from Employee 
+where empName like 'S%' and empName like '%a';
+
+--5. Write a query to find the second highest salary from the Employee table without using TOP or LIMIT.
+select max(empSalary) from Employee
+where empSalary <(select max(empSalary) from Employee);
+
+--6. Create a table Customer with fields
+create table Customer (
+	ID int primary key,
+	Name varchar(40),
+	Email varchar(50),
+	CreatedDate date default GETDATE()
+);
 
